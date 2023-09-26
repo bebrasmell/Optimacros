8aad972d31396b48babe003a6c5e8b95b925b83e
Zak <lovesomezak@gmail.com>
2023-09-26 03:46:23 +0200

    Year is int and price is positive

--- server/src/models/car.z.ts
+++ server/src/models/car.z.ts
@@ -13,8 +13,8 @@ export const zCar = z.object({
     id: zId,
     brand: z.string(), // ENUM?
     model: z.string(),
-    year: z.number().min(1900).max(currentYear + 1),
-    price: z.number().min(0),
+    year: z.number().int().min(1900).max(currentYear + 1),
+    price: z.number().positive(),
 });
 
 /**
